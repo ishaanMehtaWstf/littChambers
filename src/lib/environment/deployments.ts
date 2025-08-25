4dd3a6ae705352065e4d6e89040ac50be4dfb3cd
Sudhir Modanwal <sudhir.modanwal@thewasserstoff.com>
2025-08-25 15:15:10 +0530

    Update deployments.ts

--- src/lib/environment/deployments.ts
+++ src/lib/environment/deployments.ts
@@ -2,7 +2,8 @@ export function getDeployment() {
   console.log(process.env);
   return {
     name: "Deep Agent",
-    deploymentUrl: process.env.NEXT_PUBLIC_DEPLOYMENT_URL || "http://127.0.0.1:2024",
+    deploymentUrl: process.env.NEXT_PUBLIC_DEPLOYMENT_URL || "http://ai.litchamber.lit.law
+",
     agentId: process.env.NEXT_PUBLIC_AGENT_ID || "deepagent",
   };
 }
