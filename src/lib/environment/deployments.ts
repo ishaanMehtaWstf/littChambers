c8c53d43d4a758b5e973e80c7486144381943347
Sudhir Modanwal <sudhir.modanwal@thewasserstoff.com>
2025-08-25 15:11:38 +0530

    Update deployments.ts

--- src/lib/environment/deployments.ts
+++ src/lib/environment/deployments.ts
@@ -1,4 +1,5 @@
 export function getDeployment() {
+  console.log(process.env);
   return {
     name: "Deep Agent",
     deploymentUrl: process.env.NEXT_PUBLIC_DEPLOYMENT_URL || "http://127.0.0.1:2024",
